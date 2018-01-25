18fb388a2a85e6c10199c40a9283b1654849b531
Paulo Sergio <ps131296@gmail.com>
2018-01-25 21:42:31 +0000

    correct seed

--- seed.js
+++ seed.js
@@ -2,27 +2,16 @@
 var Campground = require("./models/campground");
 //var Comment = require("./models/comment");
 
-var campgrounds_data = [
+var mockCampground = [
   {
     name: "Riverside",
     img: "https://images.unsplash.com/photo-1476514525535-07fb3b4ae5f1?auto=format&fit=crop&w=1350&q=80",
     description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc quis nisi vehicula, consequat neque ut, finibus sapien. Aenean vulputate, libero eget lacinia consectetur, nulla purus tempor dui, quis sodales massa ipsum nec risus. Vestibulum rutrum pellentesque metus, quis euismod lacus hendrerit malesuada.",
     creator_username: "John Doe",
-    price: "9"
-  },
-  {
-    name: "Waterfalls",
-    img: "https://images.unsplash.com/photo-1442850473887-0fb77cd0b337?auto=format&fit=crop&w=1350&q=80",
-    description: "Phasellus feugiat euismod sem, et aliquam magna. Vivamus in est elementum tortor maximus pulvinar nec eu sem. Cras volutpat nulla arcu, eget lacinia erat vehicula eu. Maecenas condimentum nec nunc id tristique. Fusce cursus bibendum orci, vitae consectetur elit auctor nec. Vestibulum luctus mattis condimentum.",
-    creator_username: "John Doe",
-    price: "22"
-  },
-  {
-    name: "Canyonview",
-    img: "https://images.unsplash.com/photo-1431794062232-2a99a5431c6c?auto=format&fit=crop&w=1350&q=80",
-    description: "Sed faucibus metus sed elit volutpat ultrices. Sed lorem nibh, pharetra id sodales ac, gravida vel dui. Nullam condimentum euismod tortor a tristique. Maecenas pharetra magna nisi, nec suscipit ante congue et. Donec vehicula diam lectus, faucibus scelerisque justo iaculis non. Sed ullamcorper efficitur condimentum.",
-    creator_username: "John Doe",
-    price: "90"
+    price: "9",
+    location: 'Av. Boa Viagem, 6500 - Boa Viagem, Recife - PE, Brazil',
+    lat: -8.1455605,
+    lng: -34.9059436
   }
 ];
 
@@ -36,7 +25,7 @@ function SeedDB(){
       console.log("Campgrounds removed");
       
       //Add campgrounds after removed
-      campgrounds_data.forEach(function(campground){
+      mockCampground.forEach(function(campground){
         Campground.create(campground, function(error, campground_created){
           if (error) {
             console.log("error creating campground...");
