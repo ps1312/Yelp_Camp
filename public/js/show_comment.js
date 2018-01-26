864477d5f88cc6c98da564b1f692240cf2e3c881
Paulo Sergio <ps131296@gmail.com>
2018-01-26 17:38:27 +0000

    add bootstrap js collapse and minor changes

--- public/js/show_comment.js
+++
@@ -1,9 +0,0 @@
-var comment_field = document.getElementById("edit_field");
-var x = document.getElementById("edit_comment");
-x.addEventListener("click", function() {
-  if (comment_field.style.display === "block") {
-        comment_field.style.display = "none";
-    } else {
-        comment_field.style.display = "block";
-    }
-});
